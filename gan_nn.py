17423e24d685a4045d54e5b3e35f61804509008e
MariusLastauskas <marius20122@gmail.com>
2020-05-13 13:16:47 +0300

    gan topology configuration

--- gan_nn.py
+++ gan_nn.py
@@ -10,60 +10,54 @@ import PIL
 from tensorflow.keras import layers
 import time
 import gan_io
+import gan_utils as utils
 
 from IPython import display
 
 def make_generator_model(input_neurons_count):
+    net_config = gan_io.readConfigFile('./configuration/network-model.conf')
+
     model = tf.keras.Sequential()
-    model.add(layers.Dense(30*30*20, use_bias=False, input_shape=(input_neurons_count,)))
+    model.add(layers.Dense(utils.multiplyList(net_config['GENERATOR_INPUT_DIMENSIONS']), use_bias=False, input_shape=(utils.multiplyList(net_config['INPUT_IMAGE_DIMENSIONS']),)))
     model.add(layers.BatchNormalization())
     model.add(layers.LeakyReLU())
 
-    model.add(layers.Reshape((30, 30, 20)))
+    model.add(layers.Reshape(net_config['GENERATOR_INPUT_DIMENSIONS']))
     assert model.output_shape == (None, 30, 30, 20) # Note: None is the batch size
 
-    model.add(layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
-    assert model.output_shape == (None, 30, 30, 128)
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
-    
-    model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same', use_bias=False))
-    assert model.output_shape == (None, 15, 15, 128)
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
+    i=0
+    for neuron_count in net_config['GENERATOR_CONV_NEURONS_COUNT']:
+        stride = 1
+        if i < 2:
+            stride = 2
+            i = i + 1
+        model.add(layers.Conv2DTranspose(neuron_count, (5, 5), strides=(stride, stride), padding='same', use_bias=False))
+        # assert model.output_shape == (None, 30, 30, 128)
+        model.add(layers.BatchNormalization())
+        model.add(layers.LeakyReLU())
 
-    model.add(layers.Conv2DTranspose(128, (5, 5), strides=(2, 2), padding='same', use_bias=False))
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
-
-    model.add(layers.Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
-    assert model.output_shape == (None, 60, 60, 64)
-    model.add(layers.BatchNormalization())
-    model.add(layers.LeakyReLU())
-
-    model.add(layers.Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
+    model.add(layers.Conv2DTranspose(1, (5, 5), strides=(1, 1), padding='same', use_bias=False, activation='tanh'))
     assert model.output_shape == (None, 120, 120, 1)
 
     return model
 
 def make_discriminator_model(neuron_shape):
+    net_config = gan_io.readConfigFile('./configuration/network-model.conf')
+
+    hidden_neurons = net_config['DISCRIMINATOR_CONV_NEURONS_COUNT']
+
     model = tf.keras.Sequential()
-    model.add(layers.Conv2D(64, (5, 5), strides=(2, 2), padding='same', input_shape=neuron_shape))
+    model.add(layers.Conv2D(hidden_neurons[0], (5, 5), strides=(2, 2), padding='same', input_shape=net_config['DISCRIMINATOR_INPUT_DIMENSIONS']))
     model.add(layers.LeakyReLU())
     model.add(layers.Dropout(0.3))
 
-    model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
-    model.add(layers.LeakyReLU())
-    model.add(layers.Dropout(0.3))
-    
-    model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
-    model.add(layers.LeakyReLU())
-    model.add(layers.Dropout(0.3))
-
-
-    model.add(layers.Conv2D(128, (5, 5), strides=(2, 2), padding='same'))
-    model.add(layers.LeakyReLU())
-    model.add(layers.Dropout(0.3))
+    i = 0
+    for neurons_count in hidden_neurons:
+        if i > 0:
+            model.add(layers.Conv2D(neurons_count, (5, 5), strides=(2, 2), padding='same'))
+            model.add(layers.LeakyReLU())
+            model.add(layers.Dropout(0.3))
+        i = i + 1
 
     model.add(layers.Flatten())
     model.add(layers.Dense(1))
@@ -71,7 +65,6 @@ def make_discriminator_model(neuron_shape):
     return model
 
 cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
-# cross_entropy = tf.keras.losses.CategoricalCrossentropy(from_logits=True)
 
 def discriminator_loss(real_output, fake_output):
     real_loss = cross_entropy(tf.ones_like(real_output), real_output)
